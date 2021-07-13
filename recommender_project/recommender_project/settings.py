4da049a56bc4bc5e73cad7a3a41ca5e2c6529397
namnc <7-namnc@users.noreply.git-logteam.zingplay.com>
2021-07-13 11:33:20 +0700

    update requirement.txt

--- recommender_project/recommender_project/settings.py
+++ recommender_project/recommender_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import pymysql
+import os
 
 pymysql.install_as_MySQLdb()
 
@@ -133,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
